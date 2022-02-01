794b318bc1dcff36857adf01b644761c3afffe68
Kibels <chaosfalcon@gmail.com>
2022-02-01 13:46:34 -0700

    updates

---
+++ kubejs/server_scripts/script.js
@@ -0,0 +1,28 @@
+// priority: 0
+
+settings.logAddedRecipes = true
+settings.logRemovedRecipes = true
+settings.logSkippedRecipes = false
+settings.logErroringRecipes = true
+
+console.info('Hello, World! (You will see this line every time server resources reload)')
+
+onEvent('recipes', event => {
+	// Change recipes here
+	event.shaped('easypaxellite:copper_paxel', [
+    'CCC',
+    'CS ',
+    ' S '
+  ], {
+    C: 'minecraft:copper_ingot',
+    S: '#forge:rods/wooden'
+  })
+})
+
+onEvent('item.tags', event => {
+	// Get the #forge:cobblestone tag collection and add Diamond Ore to it
+	// event.get('forge:cobblestone').add('minecraft:diamond_ore')
+
+	// Get the #forge:cobblestone tag collection and remove Mossy Cobblestone from it
+	// event.get('forge:cobblestone').remove('minecraft:mossy_cobblestone')
+})
