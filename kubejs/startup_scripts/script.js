bc89a8ee23a3c81e889ec5598a1f713efa36865b
Kibels <chaosfalcon@gmail.com>
2022-02-05 12:56:53 -0700

    kubejs files

---
+++ kubejs/startup_scripts/script.js
@@ -0,0 +1,12 @@
+// priority: 0
+
+console.info('Hello, World! (You will only see this line once in console, during startup)')
+
+onEvent('item.registry', event => {
+	// Register new items here
+})
+
+onEvent('block.registry', event => {
+	// Register new blocks here
+	// event.create('example_block').material('wood').hardness(1.0).displayName('Example Block')
+})
